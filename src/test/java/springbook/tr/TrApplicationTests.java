ab7cc780b221c8ebe7a7795eb475fd94e131c098
minjin <sajajin115@gmail.com>
2025-02-05 09:52:11 +0900

    Setting the environment

---
+++ src/test/java/springbook/tr/TrApplicationTests.java
@@ -0,0 +1,13 @@
+package springbook.tr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
