53f9ae1f7556bbcc724c64406565f4e2c3534c56
minjin <sajajin115@gmail.com>
2025-02-11 14:02:55 +0900

    Feature: 헬스 체크 API 명세서 작성

--- src/main/java/springbook/tr/healthcheck/controller/HealthCheckController.java
+++ src/main/java/springbook/tr/healthcheck/controller/HealthCheckController.java
@@ -1,5 +1,9 @@
 package springbook.tr.healthcheck.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +15,8 @@ import springbook.tr.healthcheck.service.HealthCheckService;
 @RequestMapping("/v1/api")
 public class HealthCheckController {
 
-
+  @Operation(summary = "서버 상태 헬스체크",description = "서버가 정상적으로 운영중인지 확입합니다.")
+  @ApiResponse(responseCode = "200", description = "성공",content = @Content(schema = @Schema(implementation = HealthCheckResponse.class)))
   @GetMapping("/healthCheck")
   public ResponseEntity<Object> healthCheck() {
     HealthCheckResponse response = HealthCheckService.getServerHealthStatus();
