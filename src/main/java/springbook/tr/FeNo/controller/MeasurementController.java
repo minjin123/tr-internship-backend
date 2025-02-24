49c35053ecb65d0ca56978b59953380aed51be7e
minjin <sajajin115@gmail.com>
2025-02-24 13:07:13 +0900

    [FEATURE] 날짜별 측정 기록조회 추가 했습니다.

--- src/main/java/springbook/tr/FeNo/controller/MeasurementController.java
+++ src/main/java/springbook/tr/FeNo/controller/MeasurementController.java
@@ -2,9 +2,12 @@ package springbook.tr.FeNo.controller;
 
 import static springbook.tr.http.model.ResponseCode.*;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +24,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import springbook.tr.FeNo.Service.MeasurementRecordInquiryService;
 import springbook.tr.FeNo.Service.MeasurementService;
+import springbook.tr.FeNo.model.dto.MeasurementInquiryResponseDto;
 import springbook.tr.FeNo.model.dto.MeasurementResponseDto;
 import springbook.tr.exception.GlobalExceptionHandler;
 import springbook.tr.http.model.HttpResponseBody;
@@ -32,6 +36,7 @@ import springbook.tr.http.model.HttpResponseBody;
 public class MeasurementController {
 
 	private final MeasurementService measurementService;
+	private final MeasurementRecordInquiryService measurementRecordInquiryService;
 
 	@Operation(summary = "FeNo 검사", description = "FeNo 검사가 정상적으로 작동하는지 확인합니다.")
 	@ApiResponses({
@@ -52,5 +57,28 @@ public class MeasurementController {
 			.message(NOT_ISSUE.getMessage())
 			.response(measurementResponseDto)
 			.build();
+	}
+
+	@Operation(summary = "FeNo 검사결과 조회", description = "FeNo 검사결과 조회가 정상적으로 작동하는지 확인합니다.")
+	@ApiResponses({
+		@ApiResponse(responseCode = "200", description = "성공",
+			content = @Content(schema = @Schema(implementation = MeasurementInquiryResponseDto.class))),
+		@ApiResponse(responseCode = "404", description = "해당 날짜에 검사를 찾을 수 없습니다.",
+			content = @Content(schema = @Schema(implementation = GlobalExceptionHandler.class))),
+		@ApiResponse(responseCode = "404", description = "환자가 찾을 수 없습니다.",
+			content = @Content(schema = @Schema(implementation = GlobalExceptionHandler.class)))
+	})
+	@GetMapping("/{patientId}/measurements")
+	public ResponseEntity<Object> measurementRecordInquiry(@PathVariable Long patientId,
+		@RequestParam(required = false) String date) {
+		List<MeasurementInquiryResponseDto> measurementInquiryResponseDto = measurementRecordInquiryService
+			.getMeasurementRecord(patientId, date);
+		return HttpResponseBody.builder()
+			.code(HttpStatus.CREATED.value())
+			.subCode(NOT_ISSUE.getSubCode())
+			.message(NOT_ISSUE.getMessage())
+			.response(measurementInquiryResponseDto)
+			.build();
+
 	}
 }
