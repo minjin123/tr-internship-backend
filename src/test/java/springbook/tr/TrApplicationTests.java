275c222990bfed479b9da6b53064923337ae25c5
minjin123 <168341415+minjin123@users.noreply.github.com>
2025-02-25 17:57:32 +0900

    Update TrApplicationTests.java

--- src/test/java/springbook/tr/TrApplicationTests.java
+++ src/test/java/springbook/tr/TrApplicationTests.java
@@ -3,12 +3,6 @@ package springbook.tr;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {
-	"spring.datasource.url=",
-	"spring.datasource.username=",
-	"spring.datasource.password=",
-	"spring.jpa.database-platform="
-})
 class TrApplicationTests {
 	@Test
 	void contextLoads() {
