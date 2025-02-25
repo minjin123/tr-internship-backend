ec262394654bac63ca3a7ccbd8306d28d22f469c
minjin <sajajin115@gmail.com>
2025-02-25 17:48:14 +0900

    [ADD] 테스트 DB 실행 차단

--- src/test/java/springbook/tr/TrApplicationTests.java
+++ src/test/java/springbook/tr/TrApplicationTests.java
@@ -3,11 +3,15 @@ package springbook.tr;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+	"spring.datasource.url=",
+	"spring.datasource.username=",
+	"spring.datasource.password=",
+	"spring.jpa.database-platform="
+})
 class TrApplicationTests {
-
 	@Test
 	void contextLoads() {
+		// 테스트 코드
 	}
-
 }
